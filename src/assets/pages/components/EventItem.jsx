a637da327a2c8a5798ba37ca939176bd9729d187
Daggergoboom <matthiaslundin@gmail.com>
2025-06-07 14:07:49 +0200

    test

---
+++ src/assets/pages/components/EventItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EventItem = ({item}) => {
+  return (
+    <div className="event-card">
+        <div>{item.title}</div>
+
+    </div>
+  )
+}
+
+export default EventItem
---
+++ src/assets/pages/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header> Events </header>
+  )
+}
+
+export default Header
